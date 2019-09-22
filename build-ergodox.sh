21cec10016bdc5614b58c1967a5759bcbe4579e1
Hugo Gayosso <tribrhy@gmail.com>
2019-09-22 10:09:13 -0400

    use variable, be self-suficient.

--- build-ergodox.sh
+++ build-ergodox.sh
@@ -1,2 +1,10 @@
-cd ~/git/qmk_firmware
-docker run -e keymap=TribalNightOwl -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw -u $(id -u):$(id -g) edasque/qmk_firmware
+QMK_SRC_DIR=~/git/qmk_firmware
+QMK_KEYMAPS_DIR=${QMK_SRC_DIR}/keyboards/ergodox_ez/keymaps
+MYKEYMAP_DIR=~/git/ergodox-ez-keymap
+MYKEYMAP_NAME=TribalNightOwl
+
+rm -rf ${QMK_KEYMAPS_DIR}/${MYKEYMAP_NAME}
+cp -r  ${MYKEYMAP_DIR} ${QMK_KEYMAPS_DIR}/${MYKEYMAP_NAME}
+
+cd ${QMK_SRC_DIR}
+docker run -e keymap=${MYKEYMAP_NAME} -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw -u $(id -u):$(id -g) edasque/qmk_firmware
