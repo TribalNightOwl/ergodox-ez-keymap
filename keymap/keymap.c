cc6c22de94b8b30e4719d8f751fe66b9c89325d5
Hugo Gayosso <tribrhy@gmail.com>
2019-11-13 22:11:38 -0500

    Mac keys in left home row

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
     KC_ESC,       KC_1,  ALT_T(KC_2),  CTL_T(KC_3),  LSFT_T(KC_4),  KC_5,                         _______,
 		KC_INS,       KC_Q,         KC_W,         KC_E,          KC_R,  KC_T,     TD(CT_SQTE_DQTE_BQTE_CARET),
-    KC_DELT,      KC_A,  ALT_T(KC_S),  CTL_T(KC_D),  LSFT_T(KC_F),  KC_G,
+    KC_DELT,      KC_A,  ALT_T(KC_S), LCMD_T(KC_D),  LSFT_T(KC_F),  KC_G,
 		_______,      KC_Z,         KC_X,         KC_C,          KC_V,  KC_B,     TD(CT_LPRN_LBRC_LCBR_LTHAN),
 		_______,   _______,       KC_ESC,     TT(MOVE),      TT(NUMB),
 	                                                                                                _______,  _______,
