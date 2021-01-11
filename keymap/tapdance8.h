49d27294ca5d3af56084bc4a6e4cf8e26fde1cd8
Hugo Gayosso <tribrhy@gmail.com>
2021-01-11 18:12:25 -0500

    change RCTL to LCTL for all combinations

--- keymap/tapdance8.h
+++ keymap/tapdance8.h
@@ -15,10 +15,10 @@ static tap dance8_state = {
 void dance8_finished (qk_tap_dance_state_t *state, void *user_data) {
   dance8_state.state = cur_dance(state);
   switch (dance8_state.state) {
-    case SINGLE_TAP:  register_code(KC_RCTL); register_code(KC_B); break;
-    case SINGLE_HOLD: register_code(KC_RCTL); register_code(KC_B); unregister_code(KC_RCTL); unregister_code(KC_B); register_code(KC_LBRACKET); break;
+    case SINGLE_TAP:  register_code(KC_LCTL); register_code(KC_B); break;
+    case SINGLE_HOLD: register_code(KC_LCTL); register_code(KC_B); unregister_code(KC_LCTL); unregister_code(KC_B); register_code(KC_LBRACKET); break;
     case DOUBLE_TAP:  register_code(KC_LCTL); register_code(KC_W); break;
-    case DOUBLE_HOLD: register_code(KC_RCTL); register_code(KC_B); unregister_code(KC_RCTL); unregister_code(KC_B); register_code(KC_RBRACKET); break;
+    case DOUBLE_HOLD: register_code(KC_LCTL); register_code(KC_B); unregister_code(KC_LCTL); unregister_code(KC_B); register_code(KC_RBRACKET); break;
     case DOUBLE_SINGLE_TAP: register_code(KC_NO);
     //Last case is for fast typing. Assuming your key is `f`:
     //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
@@ -28,7 +28,7 @@ void dance8_finished (qk_tap_dance_state_t *state, void *user_data) {
 
 void dance8_reset (qk_tap_dance_state_t *state, void *user_data) {
   switch (dance8_state.state) {
-    case SINGLE_TAP:  unregister_code(KC_RCTL); unregister_code(KC_B); break;
+    case SINGLE_TAP:  unregister_code(KC_LCTL); unregister_code(KC_B); break;
     case SINGLE_HOLD: unregister_code(KC_LBRACKET); break;
     case DOUBLE_TAP:  unregister_code(KC_LCTL); unregister_code(KC_W); break;
     case DOUBLE_HOLD: unregister_code(KC_RBRACKET); break;
