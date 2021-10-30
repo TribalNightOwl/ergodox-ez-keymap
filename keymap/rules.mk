9443f9aad14494dc1b26b61f44ba15db9d1a1d8b
Hugo Gayosso <tribrhy@gmail.com>
2021-10-29 21:47:14 -0400

    update qmk breaking changes

--- keymap/rules.mk
+++ keymap/rules.mk
@@ -1,7 +1,7 @@
-TAP_DANCE_ENABLE  = yes
+TAP_DANCE_ENABLE = yes
 RGBLIGHT_ENABLE = yes
-DEBOUNCE_TYPE = sym_g
-BOOTMAGIC_ENABLE = full
+DEBOUNCE_TYPE = sym_defer_g
+BOOTMAGIC_ENABLE = yes
 
 ifndef QUANTUM_DIR
     include ../../../../Makefile
