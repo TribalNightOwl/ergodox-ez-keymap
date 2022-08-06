107c2c578e379fc53fe1dc57732585c836f56390
Hugo Gayosso <tribrhy@gmail.com>
2022-08-06 14:19:17 -0400

    use TribalNightOwl-ergodox-ez branch

--- ergodox.sh
+++ ergodox.sh
@@ -2,6 +2,7 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BASE_DIR="${HOME}/git"
+QMK_BRANCH="TribalNightOwl-ergodox-ez"
 QMK_SRC_DIR="${BASE_DIR}/qmk_firmware"
 QMK_REPO="git@github.com:TribalNightOwl/qmk_firmware.git"
 MYKEYMAP_NAME="TribalNightOwl"
@@ -11,6 +12,14 @@ clone(){
     cd "${BASE_DIR}" || exit
     rm -rf "${QMK_SRC_DIR}"
     git clone --recurse-submodules "${QMK_REPO}"
+    cd "${QMK_SRC_DIR}" || exit
+    git checkout "${QMK_BRANCH}"
+}
+
+clean(){
+    cd "${QMK_SRC_DIR}" || exit
+    git clean -fd
+    git checkout "${QMK_BRANCH}"
 }
 
 update(){
